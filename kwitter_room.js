02fd54c327f2810feb1ce5e41ba0645d5c393124
Hanshal15 <77623196+Hanshal15@users.noreply.github.com>
2021-05-08 16:18:19 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+
+var firebaseConfig = {
+      apiKey: "AIzaSyC2XBP4fYa3vZab8mAx9HiPGvNo99-Fi6w",
+      authDomain: "kwitter-f7bbe.firebaseapp.com",
+      databaseURL: "https://kwitter-f7bbe-default-rtdb.firebaseio.com",
+      projectId: "kwitter-f7bbe",
+      storageBucket: "kwitter-f7bbe.appspot.com",
+      messagingSenderId: "747071201226",
+      appId: "1:747071201226:web:202aecf6ff2d61d462b933",
+
+    };
+    firebase.initializeApp(firebaseConfig);
+  
